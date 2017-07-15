8d4b605fec11399ad57804ff3b6dd6cd59db59c2
Naishadh Parmar <naishadh_parmar@live.com>
2017-07-15 09:52:06 +0530

    Example

--- app/src/main/java/org/naishadhparmar/customcalendar/ActivityMain.java
+++ app/src/main/java/org/naishadhparmar/customcalendar/ActivityMain.java
@@ -1,5 +1,6 @@
 package org.naishadhparmar.customcalendar;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,12 +8,14 @@ import android.support.design.widget.Snackbar;
 
 import org.naishadhparmar.zcustomcalendar.CustomCalendar;
 import org.naishadhparmar.zcustomcalendar.OnDateSelectedListener;
+import org.naishadhparmar.zcustomcalendar.OnNavigationButtonClickedListener;
 import org.naishadhparmar.zcustomcalendar.Property;
 
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Map;
 
-public class ActivityMain extends AppCompatActivity {
+public class ActivityMain extends AppCompatActivity implements OnNavigationButtonClickedListener{
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +53,8 @@ public class ActivityMain extends AppCompatActivity {
 		mapDateToDesc.put(20, "holiday");
 		mapDateToDesc.put(24, "unavailable");
 		
+		customCalendar.setOnNavigationButtonClickedListener(CustomCalendar.PREVIOUS, this);
+		customCalendar.setOnNavigationButtonClickedListener(CustomCalendar.NEXT, this);
 		customCalendar.setDate(calendar, mapDateToDesc);
 		customCalendar.setOnDateSelectedListener(new OnDateSelectedListener() {
 			@Override
@@ -57,5 +62,27 @@ public class ActivityMain extends AppCompatActivity {
 				Snackbar.make(customCalendar, selectedDate.get(Calendar.DAY_OF_MONTH) + " selected", Snackbar.LENGTH_LONG).show();
 			}
 		});
+	}
+	
+	@Override
+	public Map<Integer, Object>[] onNavigationButtonClicked(int whichButton, Calendar newMonth) {
+		Map<Integer, Object>[] arr = new Map[2];
+		switch(newMonth.get(Calendar.MONTH)) {
+			case Calendar.AUGUST:
+				arr[0] = new HashMap<>(); //This is the map linking a date to its description
+				arr[0].put(3, "unavailable");
+				arr[0].put(6, "holiday");
+				arr[0].put(21, "unavailable");
+				arr[0].put(24, "holiday");
+				arr[1] = null; //Optional: This is the map linking a date to its tag.
+				break;
+			case Calendar.JUNE:
+				arr[0] = new HashMap<>();
+				arr[0].put(5, "unavailable");
+				arr[0].put(10, "holiday");
+				arr[0].put(19, "holiday");
+				break;
+		}
+		return arr;
 	}
 }
